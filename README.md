dcf361a98785c18873a9a63c6b0d237033046808
Oliver Joseph Ash <oliverjash@gmail.com>
2017-09-30 16:47:46 +0100

    Init

---
+++ README.md
@@ -0,0 +1,17 @@
+```
+$ yarn
+$ webpack
+ts-loader: Using typescript@2.5.3 and /Users/OliverJAsh/Development/temp/webpack-ts-loader-shared/src/browser/tsconfig.json
+ts-loader: Using typescript@2.5.3 and /Users/OliverJAsh/Development/temp/webpack-ts-loader-shared/src/service-worker/tsconfig.json
+Hash: e1ec6653826557ea6756
+Version: webpack 3.6.0
+Time: 1063ms
+                Asset     Size  Chunks             Chunk Names
+    service-worker.js  2.83 kB       0  [emitted]  service-worker
+           browser.js  2.81 kB       1  [emitted]  browser
+service-worker.js.map  3.04 kB       0  [emitted]  service-worker
+       browser.js.map  3.01 kB       1  [emitted]  browser
+   [0] ./src/shared/index.ts 68 bytes {0} {1} [built]
+   [1] ./src/browser/index.ts 126 bytes {1} [built] [1 error]
+   [2] ./src/service-worker/index.ts 138 bytes {0} [built]
+```
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ROOT_PATH = path.join(__dirname, '.');
+const TARGET_PATH = path.join(ROOT_PATH, './target-webpack');
+
+const SHARED_SOURCE_PATH = path.join(ROOT_PATH, './src/shared');
+const BROWSER_SOURCE_PATH = path.join(ROOT_PATH, './src/browser');
+const SERVICE_WORKER_SOURCE_PATH = path.join(ROOT_PATH, './src/service-worker');
+
+const config = {
+    devtool: 'source-map',
+    entry: {
+        browser: path.join(BROWSER_SOURCE_PATH, './index.ts'),
+        'service-worker': path.join(SERVICE_WORKER_SOURCE_PATH, './index.ts'),
+    },
+    output: {
+        path: TARGET_PATH,
+        filename: '[name].js',
+    },
+    resolve: {
+        extensions: [
+            // start defaults
+            '.js',
+            '.json',
+            // end defaults
+            '.ts',
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                include: [BROWSER_SOURCE_PATH, SHARED_SOURCE_PATH],
+                loader: 'ts-loader',
+                options: {
+                    instance: 'browser',
+                    configFile: path.join(BROWSER_SOURCE_PATH, 'tsconfig.json'),
+                },
+            },
+            {
+                test: /\.ts$/,
+                include: [SERVICE_WORKER_SOURCE_PATH, SHARED_SOURCE_PATH],
+                loader: 'ts-loader',
+                options: {
+                    instance: 'service-worker',
+                    configFile: path.join(SERVICE_WORKER_SOURCE_PATH, 'tsconfig.json'),
+                },
+            },
+        ],
+    },
+};
+
+module.exports = config;
