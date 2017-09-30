2bf571af2e01c8c34d16cc663ba0ccf298b14806
Oliver Joseph Ash <oliverjash@gmail.com>
2017-09-30 17:14:12 +0100

    shared type check example

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,1 +1,8 @@
 export const shared = 'shared';
+
+// This should be type checked against both TS configs.
+// "browser" has enabled "noImplicitAny", so this should fail for "browser", but pass for
+// "service-worker".
+const fn = (a) => {
+
+}
